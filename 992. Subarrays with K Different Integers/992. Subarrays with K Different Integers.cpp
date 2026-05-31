c6c92028b15322e11d46ee576a5ce8bb95021fd8
Harsh Parvani <157064040+HarshParvani@users.noreply.github.com>
2026-05-31 15:10:37 +0530

    Add solution: 992. Subarrays with K Different Integers

---
+++ 992. Subarrays with K Different Integers/992. Subarrays with K Different Integers.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem: 992. Subarrays with K Different Integers
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/subarrays-with-k-different-integers/submissions/2018031989/
+ * Language: cpp
+ * Date: 2026-05-31
+ */
+
+class Solution {
+public:
+
+    int atMostK(vector<int>& nums, int k) {
+
+        unordered_map<int,int> mp;
+
+        int left = 0;
+        int count = 0;
+
+        for(int right = 0; right < nums.size(); right++) {
+
+            mp[nums[right]]++;
+
+            while(mp.size() > k) {
+
+                mp[nums[left]]--;
+
+                if(mp[nums[left]] == 0) {
+                    mp.erase(nums[left]);
+                }
+
+                left++;
+            }
+
+            count += (right - left + 1);
+        }
+
+        return count;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
+};
