9fed63f311dbce0c388dd826d27fb999fce5da8f
Harsh Parvani <157064040+HarshParvani@users.noreply.github.com>
2026-06-02 14:47:57 +0530

    Update solution: 1658. Minimum Operations to Reduce X to Zero

--- 1658. Minimum Operations to Reduce X to Zero/1658. Minimum Operations to Reduce X to Zero.cpp
+++ 1658. Minimum Operations to Reduce X to Zero/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 1658. Minimum Operations to Reduce X to Zero
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/submissions/2019999139/
+ * Link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/submissions/2020002075/
  * Language: cpp
  * Date: 2026-06-02
  */
@@ -9,25 +9,24 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-    int n=nums.size();
-    int totalsum =0;
-    for (int i:nums)
-    totalsum+=i;
-    int reqsum=totalsum-x;
-    int winsum=0;
-    int len=-1;
-    int left =0,right=0;
-    while (right <n){
-        winsum+=nums[right];
-        while(left<=right && winsum>reqsum){
-            winsum-=nums[left];
-            left++;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int target = total - x;
+        if (target < 0) return -1;
+
+        int n = nums.size();
+        int left = 0, currSum = 0, maxLen = -1;
+
+        for (int right = 0; right < n; right++) {
+            currSum += nums[right];
+            while (left <= right && currSum > target) {
+                currSum -= nums[left++];
+            }
+            if (currSum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
         }
-        if(winsum==reqsum){
-            len = max(right-left+1,len);
-        }
-        right++;
-    }
-    return len == -1?-1:n-len;
+
+        return (maxLen == -1) ? -1 : n - maxLen;
     }
 };
+
