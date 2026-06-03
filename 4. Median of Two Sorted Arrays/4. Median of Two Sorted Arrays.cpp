9d73dd8a1002c2a284db8478ce17e610dd859222
Harsh Parvani <157064040+HarshParvani@users.noreply.github.com>
2026-06-03 14:55:27 +0530

    Update solution: 4. Median of Two Sorted Arrays

--- 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
@@ -1,45 +1,59 @@
 /*
  * Problem: 4. Median of Two Sorted Arrays
  * Difficulty: Hard
- * Link: https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/2021003101/
+ * Link: https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/2021077738/
  * Language: cpp
  * Date: 2026-06-03
  */
 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1,
-                                  vector<int>& nums2) {
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-        vector<int> arr;
+        int n = nums1.size();
+        int m = nums2.size();
 
-        int i = 0;
-        int j = 0;
+        // Always binary search on smaller array
+        if(n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        while(i < nums1.size() && j < nums2.size()) {
+        int low = 0;
+        int high = n;
 
-            if(nums1[i] <= nums2[j]) {
-                arr.push_back(nums1[i++]);
+        int total = n + m;
+        int half = (total + 1) / 2;
+
+        while(low <= high) {
+
+            int i = low + (high - low) / 2;
+            int j = half - i;
+
+            int left1  = (i == 0) ? INT_MIN : nums1[i - 1];
+            int right1 = (i == n) ? INT_MAX : nums1[i];
+
+            int left2  = (j == 0) ? INT_MIN : nums2[j - 1];
+            int right2 = (j == m) ? INT_MAX : nums2[j];
+
+            if(left1 <= right2 && left2 <= right1) {
+
+                if(total % 2 == 0) {
+                    return (max(left1, left2) +
+                            min(right1, right2)) / 2.0;
+                }
+
+                return max(left1, left2);
             }
+
+            else if(left1 > right2) {
+                high = i - 1;
+            }
+
             else {
-                arr.push_back(nums2[j++]);
+                low = i + 1;
             }
         }
 
-        while(i < nums1.size()) {
-            arr.push_back(nums1[i++]);
-        }
-
-        while(j < nums2.size()) {
-            arr.push_back(nums2[j++]);
-        }
-
-        int n = arr.size();
-
-        if(n % 2 == 1) {
-            return arr[n/2];
-        }
-
-        return (arr[n/2] + arr[n/2 - 1]) / 2.0;
+        return 0.0;
     }
 };
